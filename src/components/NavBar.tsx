fb922feadf5426efde86d86b0b54505af2b25e5e
Jun Jie Ng <ngjunjie13@gmail.com>
2025-05-09 23:03:43 +1000

    feat: adding NavBar to layout

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <ul className="flex space-x-4">
+        <li>
+          <Link href="/clip-corner" className="text-white hover:text-gray-300">
+            Clip Corner
+          </Link>
+        </li>
+        <li>
+        <Link href="/cubing-corner" className="text-white hover:text-gray-300">
+            Cubing Corner
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
