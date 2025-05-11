5a225930f70b001b01d2904f7e30914f526e0815
Jun Jie Ng <ngjunjie13@gmail.com>
2025-05-11 23:09:14 +1000

    fix: typing

--- src/app/clip-corner/page.tsx
+++ src/app/clip-corner/page.tsx
@@ -5,9 +5,22 @@ import VideoCarousel from "@/components/VideoCarousel";
 import { useEffect, useState } from "react";
 
 export default function ClipCornerPage() {
-  const [videos, setVideos] = useState<any[]>([]);
+  const [videos, setVideos] = useState<formattedVideo[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  interface VideoObject {
+    key: string;
+    version: string;
+    uploaded: string | number;
+  }
+  interface formattedVideo {
+    id: string;
+    title: string;
+    date: string;
+    url: string;
+    thumbnail?: string;
+  }
+  
   useEffect(() => {
     fetch("https://video-worker.ngjunjie13.workers.dev/list")
       .then((res) => {
@@ -17,17 +30,20 @@ export default function ClipCornerPage() {
         return res.json();
       })
       .then((data) => {
-        const formattedVideos = data.objects.map((video: any) => {
+        const formattedVideos = data.objects.map((video: VideoObject) => {
           return {
             // url: `https://video-worker.ngjunjie13.workers.dev/${video.key}`,
             url: `https://www.cdn.jumanji.work/${video.key}`, //getting the video from the CDN directly
             id: video.version,
-            title: video.key.replace(/\.mp4$/, '').replace(/_/g, ' ').replace(/^\w/, (c: string) => c.toUpperCase()),
+            title: video.key
+              .replace(/\.mp4$/, "")
+              .replace(/_/g, " ")
+              .replace(/^\w/, (c: string) => c.toUpperCase()),
             thumbnail: "", // Add a default thumbnail or modify as needed
-            date: new Date(video.uploaded).toLocaleString()
+            date: new Date(video.uploaded).toLocaleString(),
           };
         });
-        console.log("Response:",formattedVideos);
+        console.log("Response:", formattedVideos);
         setVideos(formattedVideos);
         setIsLoading(false);
       })
