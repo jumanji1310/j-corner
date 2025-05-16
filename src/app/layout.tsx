102fbf68d69c9e8f99934c71fcf09abdd12909f2
Jun Jie Ng <ngjunjie13@gmail.com>
2025-05-16 23:43:45 +1000

    feat: use Lato font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
 import NavBar from "@/components/NavBar"; // Adjust the path based on your project structure
+import { Lato, Inter } from 'next/font/google';
+
+// Initialize the font
+const font = Lato({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={font.className}>
       <body>
         <NavBar />
         {children}
