8af675bb26357098edd3b5b2a73ecec5b9a5059e
Jun Jie Ng <ngjunjie13@gmail.com>
2025-05-17 18:06:50 +1000

    fix: use img over Image

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,14 +1,11 @@
-import Image from "next/image";
-
 export default function LoadingScreen({ src }: { src: string }) {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black">
-      <Image
+      <img
         src={src}
         alt="Loading animation"
         width={300}
         height={300}
-        priority
       />
     </div>
   );
