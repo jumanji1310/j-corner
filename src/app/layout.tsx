edec916d7b230091579c1de1f349366cc4eb614c
Jun Jie Ng <ngjunjie13@gmail.com>
2025-06-29 18:53:36 +1000

    feat: vercel support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import NavBar from "@/components/NavBar"; // Adjust the path based on your proje
 import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 import { Moderustic } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 
 const moderustic = Moderustic({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class">
           <NavBar />
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
